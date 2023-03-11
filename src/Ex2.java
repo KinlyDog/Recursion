6ead5a2ef80253e3f1398f14a31e2fdea1cb6b35
KinlyDog <kinlydog@gmail.com>
2023-03-11 18:37:02 +1000

    Delete main

--- src/Ex2.java
+++ src/Ex2.java
@@ -4,17 +4,4 @@ public class Ex2 {
 
         return number % 10 + sumOfDigits(number / 10);
     }
-
-    public static void main(String[] args) {
-        int num = 1235324;
-        int sum = 0;
-
-        while (num > 0) {
-            sum += num % 10;
-            num /= 10;
-        }
-        System.out.println(sum);
-
-        System.out.println(sumOfDigits(1235324));
-    }
 }
