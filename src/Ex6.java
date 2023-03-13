a545651c6eeb21ea00024bd36e6052dc9377a35a
KinlyDog <kinlydog@gmail.com>
2023-03-14 01:47:22 +1000

    Add Ex.6

--- src/Ex6.java
+++ src/Ex6.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+
 public class Ex6 {
+    public static void evenIndex(ArrayList<Integer> list) {
+        int index = 0;
+
+        evenIndexRec(list, index);
+    }
+
+    public static void evenIndexRec(ArrayList<Integer> list, int index) {
+        if (index >= list.size()) return;
+
+        System.out.println(list.get(index));
+
+        evenIndexRec(list, index + 2);
+    }
 }
