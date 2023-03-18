d3b854de8760207a9ff4cd34e97a0db19ab4b708
KinlyDog <kinlydog@gmail.com>
2023-03-18 21:34:16 +1000

    Update Ex.8

--- src/Ex8.java
+++ src/Ex8.java
@@ -1,2 +1,34 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+
 public class Ex8 {
+    public static ArrayList<File> allFilesInCatalog(File root, int depth) {
+        ArrayList<File> expand = new ArrayList<>();
+        expand.add(root);
+
+        ArrayList<File> allFiles = new ArrayList<>();
+
+        return allFilesInCatalogRec(expand, allFiles, depth);
+    }
+
+    public static ArrayList<File> allFilesInCatalogRec(ArrayList<File> expand, ArrayList<File> allFiles, int depth) {
+        if (depth <= 0) return allFiles;
+
+        File[] expandCopy = expand.toArray(new File[0]);
+        expand.clear();
+
+        for (File file : expandCopy) {
+            if (file.isFile()) {
+                allFiles.add(file);
+            }
+
+            if (file.isDirectory()) {
+                expand.addAll(Arrays.asList(Objects.requireNonNull(file.listFiles())));
+            }
+        }
+
+        return allFilesInCatalogRec(expand, allFiles, depth - 1);
+    }
 }
