e0b0497c9850b12b5f45dcc82f595b367d819664
KinlyDog <kinlydog@gmail.com>
2023-03-18 15:34:34 +1000

    Update Ex.7

--- src/Ex7.java
+++ src/Ex7.java
@@ -3,51 +3,30 @@ import java.util.Random;
 
 public class Ex7 {
     public static int secondMaxSearch(ArrayList<Integer> list) {
-        if (list.size() == 1) {
-            return list.get(0);
+        int max1;
+        int max2;
+
+        if (list.get(0) > list.get(1)) {
+            max1 = list.get(0);
+            max2 = list.get(1);
+        } else {
+            max1 = list.get(1);
+            max2 = list.get(0);
         }
 
-        ArrayList<Integer> sortedList = new ArrayList<>(list);
-
-        secondMaxSearchRec(sortedList, 0, false);
-
-        return sortedList.get(1);
+        return secondMaxSearchRec(list, max1, max2, 2);
     }
 
-    public static void secondMaxSearchRec(ArrayList<Integer> list, int i, boolean flag) {
-        if (i > list.size() - 2 && !flag) {
-            return;
+    public static int secondMaxSearchRec(ArrayList<Integer> list, int max1, int max2, int i) {
+        if (i > list.size() - 1) return max2;
+
+        if (list.get(i) >= max1) {
+            max2 = max1;
+            max1 = list.get(i);
+        } else if (list.get(i) > max2) {
+            max2 = list.get(i);
         }
 
-        if (i > list.size() - 2 && flag) {
-            i = 0;
-            flag = false;
-        }
-
-        if (list.get(i + 1) > list.get(i)) {
-            flag = true;
-
-            int t = list.get(i);
-
-            list.set(i, list.get(i + 1));
-            list.set(i + 1, t);
-        }
-
-
-        secondMaxSearchRec(list, i + 1, flag);
-    }
-
-    public static void main(String[] args) {
-        Random rand = new Random();
-
-        ArrayList<Integer> list = new ArrayList<>();
-
-        for (int i = 0; i < 10; i++) {
-            list.add(rand.nextInt(10) + 1);
-        }
-
-        System.out.println(secondMaxSearch(list));
-
-
+        return secondMaxSearchRec(list, max1, max2, i + 1);
     }
 }
